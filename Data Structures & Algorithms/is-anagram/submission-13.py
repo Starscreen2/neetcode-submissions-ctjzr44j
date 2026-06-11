345d3dfcdfff89ce7f055a492c5b65448ac7a66a
Puyang He <128939003+Starscreen2@users.noreply.github.com>
2026-06-11 15:51:44 -0400

    Add: is-anagram - submission-13

---
+++ Data Structures & Algorithms/is-anagram/submission-13.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        ss = {}
+        tt = {}
+        
+        for i in s:
+            if i in ss:
+                ss[i] += 1
+            else:
+                ss[i] = 1
+        
+        for j in t:
+            if j in tt:
+                tt[j] += 1
+            else:
+                tt[j] = 1
+
+        return ss == tt
